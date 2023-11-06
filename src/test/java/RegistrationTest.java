c8538dd0f56a7a84899143b42d967ca943abaaef
ilyapesin <pesinilya@gmail.com>
2023-11-06 22:32:20 +0200

    CW5

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -9,18 +9,38 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class LoginTestes {
+public class RegistrationTest {
     WebDriver wd;
     @BeforeMethod
-    public void init(){
-        wd=new ChromeDriver();
+    public void init() {
+        wd = new ChromeDriver();
         wd.navigate().to("https://telranedu.web.app/home");
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
-
-   @Test
-    public void loginPositiveTest() {
+    @Test
+    public void registrationPositiveTest(){
         // open login form
+        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
+
+        //fill login form
+        int i=(int)(System.currentTimeMillis()/1000)%3600;
+        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("vasya_pupkin"+i+"@gmail.com");
+
+        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
+        passInput.click();
+        passInput.clear();
+        passInput.sendKeys("Vp12345$");
+        //click on button Registration
+        wd.findElement(By.xpath("//button[2]")).click();
+        //Assert
+        // Assert.assertTrue(wd.findElements(By.xpath("//*[.='Sign Out']")).size()>=0);
+        Assert.assertTrue(wd.findElements(By.xpath("//button")).size()>0);
+    }
+    @Test
+    public void registrationNegativeTest(){
         wd.findElement(By.xpath("//*[.='LOGIN']")).click();
 
         //fill login form
@@ -33,29 +53,8 @@ public class LoginTestes {
         passInput.click();
         passInput.clear();
         passInput.sendKeys("Vp12345$");
-        //click on button Login
-        wd.findElement(By.xpath("//button[1]")).click();
-        //Assert
-       // Assert.assertTrue(wd.findElements(By.xpath("//*[.='Sign Out']")).size()>=0);
-        Assert.assertTrue(wd.findElements(By.xpath("//button")).size()>0);
-
-    }
-    @Test
-    public void loginNegativeTestWrongEmail(){
-        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-
-        //fill login form
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("vasya_pupkingmail.com");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("Vp12345$");
-        //click on button Login
-        wd.findElement(By.xpath("//button[1]")).click();
+        //click on button Registration
+        wd.findElement(By.xpath("//button[2]")).click();
     }
 
     @AfterMethod
